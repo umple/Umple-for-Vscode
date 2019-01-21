e18f0ac713fe367624ccaaec29adf56ac2b91a82
Zuraiz Zafar <zzafa015@uottawa.ca>
2019-01-21 11:18:28 -0500

    tests should fail

--- src/test/umpleMain.ts
+++ src/test/umpleMain.ts
@@ -16,7 +16,7 @@ suite("Extension Tests", function () {
 
     // Defines a Mocha unit test
     test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(3));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
 });
