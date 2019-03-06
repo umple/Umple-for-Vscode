52b44a2ae66e8c733be4f67c3190fdeb23327e26
digized <Zuraiz.zafar97@gmail.com>
2019-03-05 19:42:26 -0500

    add multiple warning support per file

--- src/umple/actions/lint.ts
+++ src/umple/actions/lint.ts
@@ -1,5 +1,5 @@
 import { TextDocument, Uri, workspace } from "vscode";
-import { umpleAPI, Result } from "../umpleAPI";
+import { umpleAPI } from "../umpleAPI";
 import { umpleLint } from "../../helpers/UmpleLintingProvider";
 
 export async function lint(textDocument: TextDocument | Uri) {
@@ -13,13 +13,7 @@ export async function lint(textDocument: TextDocument | Uri) {
     }
 
     // will always be a uri here
-    try {
-        const res = await umpleAPI.generate(textDocument, "Java", workspace.asRelativePath("build"));
-        umpleLint.clearFile(textDocument);
-        umpleLint.lintFile(textDocument, res);
-
-    } catch (e) {
-        const err = e as Result[];
-        umpleLint.lintFile(textDocument, err)
-    }
+    const res = await umpleAPI.generate(textDocument, "Java", workspace.asRelativePath("build"));
+    umpleLint.clearFile(textDocument);
+    umpleLint.lintFile(textDocument, res);
 }
